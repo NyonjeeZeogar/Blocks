5215590242227f33c4748841ce4e226c75fd4eb4
Nyonjee Zeogar <54722046+NyonjeeZeogar@users.noreply.github.com>
2022-11-01 01:47:45 -0500

    Define plugin in php file

---
+++ app/public/wp-content/plugins/firstblock/firstblock.php
@@ -0,0 +1,8 @@
+<?php
+/**
+    * Plugin Name: First Block
+    * Plugin URI: https://github.com/NyonjeeZeogar
+    * Description: First block
+    * Author: Nyonjee Zeogar
+    * Author URI: https://github.com/NyonjeeZeogar
+*/
